c33522622190613e6a860913431eaa380d797642
Karthik425525 <83346319+Karthik425525@users.noreply.github.com>
2021-06-02 20:42:19 +0530

    Initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript And Reactjs</h1>
+      <p>I like This type of bootcamps conducted by SHAPEAI </p>
+    </div>
+  );
+}
+
+export default Note;
